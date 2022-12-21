3ab9c10a0ae2d2328af0c85e14ab5d01d94786b1
Chase Helgenlechner <chase@helgenlechner.com>
2022-12-21 11:31:41 +0100

    Started work on SQLController.

---
+++ Models/SQLController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rent_A_Ski.Models
+{
+    public class SQLController
+    {
+        #region Connection info for Windows Authentication
+        string connectionString =
+            "Server=CREATOR-DESKTOP\\SQLEXPRESS; " +
+            "Database=RENT_A_SKI; " +
+            "Trusted_Connection=True;"; //Integrated Security = true; // also works
+        #endregion
+
+        SqlConnection connection;
+        string query;
+        SqlCommand command;
+        SqlDataReader reader;
+
+        public SQLController()
+        {
+            connection = new(connectionString);
+        }
+
+    }
+}
